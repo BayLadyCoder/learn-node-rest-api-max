53f36cead7ca40d5552b569520b17d837b819488
BayLadyCoder <rachada_32@hotmail.com>
2024-01-20 18:17:21 -0500

    Fix CORS error

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const PORT = 8080;
 // app.use(express.urlencoded()); // x-www-form-urlencoded <form></form>
 app.use(express.json()); // application/json
 
+app.use((req, res, next) => {
+  //   res.setHeader('Access-Control-Allow-Origin', 'https://cdpn.io');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // allow requesting code from any origin to access the resource
+  res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/api', postsRoutes);
 
 app.listen(PORT, () => {
