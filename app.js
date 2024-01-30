499a823f0b9810db03a044b431391b4708ffcd51
BayLadyCoder <rachada_32@hotmail.com>
2024-01-29 20:24:25 -0500

    Add multer to middleware for uploading image files

--- app.js
+++ app.js
@@ -3,15 +3,37 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const multer = require('multer');
 
 const postsRoutes = require('./routes/posts');
 
 const app = express();
 const PORT = 8080;
+const fileStorage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jgp' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 // app.use(express.urlencoded({ extended: true })); // application/x-www-form-urlencoded <form></form>
 app.use(express.json()); // application/json
 app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(multer({ storage: fileStorage, fileFilter }).single('image')); //
 
 app.use((req, res, next) => {
   //   res.setHeader('Access-Control-Allow-Origin', 'https://cdpn.io');
