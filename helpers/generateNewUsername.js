ab0a7e10b991cbd01022e1965e5fd201910978cd
BayLadyCoder <rachada_32@hotmail.com>
2024-02-04 16:55:24 -0500

    Create generateNewUsername function for new users

---
+++ helpers/generateNewUsername.js
@@ -0,0 +1,24 @@
+const uniqueWords = require('../uniqueWords');
+const User = require('../models/user');
+
+const generateRandomIndex = (length = 1) => {
+  return Math.floor(Math.random() * length);
+};
+
+const generateRandomUsername = () => {
+  const randomNumber = Math.floor(Math.random().toFixed(5) * 100000);
+  const name1 = uniqueWords[generateRandomIndex(uniqueWords.length)];
+  const name2 = uniqueWords[generateRandomIndex(uniqueWords.length)];
+  const username = `${name1}-${name2}${randomNumber}`;
+  return username;
+};
+
+exports.generateNewUsername = () => {
+  const username = generateRandomUsername();
+  return User.find({ username }).then((user) => {
+    if (user.length > 0) {
+      return generateNewUsername();
+    }
+    return username;
+  });
+};
