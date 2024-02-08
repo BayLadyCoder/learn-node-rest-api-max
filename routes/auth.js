c8cecd676463c09ce05fef52be624dc79e7b804c
BayLadyCoder <rachada_32@hotmail.com>
2024-02-08 17:37:02 -0500

    Update auth route validations

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,10 @@ router.post(
         });
       })
       .normalizeEmail(),
-    body('password').trim().isLength({ min: 5, max: 30 }),
+    body('password')
+      .trim()
+      .isLength({ min: 2, max: 30 })
+      .withMessage('Password must be between 2-30 characters.'),
   ],
   authController.signUp
 );
@@ -29,7 +32,7 @@ router.post(
   '/auth/login',
   [
     body('email').isEmail().withMessage('Invalid email.').normalizeEmail(),
-    body('password').trim().isLength({ min: 5, max: 30 }),
+    body('password').trim().isLength({ min: 2, max: 30 }),
   ],
   authController.login
 );
