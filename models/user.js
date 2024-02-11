dec271a3b164e3e57f1e523b287310b926ee8b56
BayLadyCoder <rachada_32@hotmail.com>
2024-02-10 22:52:23 -0500

    Add timestamps to User model for cake day

--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  isOnline: { type: Boolean, default: false },
-  posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
-});
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    isOnline: { type: Boolean, default: false },
+    posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+  },
+  { timestamps: true } // auto generate createdAt and updatedAt
+);
 
 module.exports = mongoose.model('User', userSchema);
