4be7c934519efeef1f0176fc9603f21fd7622965
BayLadyCoder <rachada_32@hotmail.com>
2024-03-08 23:46:45 -0500

    Add PUT /comments/:commentId route

--- routes/comments.js
+++ routes/comments.js
@@ -19,6 +19,18 @@ router.post(
   commentsController.createComment
 );
 
+router.put(
+  '/comments/:commentId',
+  isAuth,
+  [
+    body('comment', 'Maximum 500 characters for a comment.')
+      .trim()
+      .isLength({ min: 1, max: 500 }),
+    body('authorId', 'authorId is  required.').notEmpty(),
+    body('postId', 'postId is required.').notEmpty(),
+  ],
+  commentsController.updateComment
+);
 router.delete('/comments/:commentId', isAuth, commentsController.deleteComment);
 
 module.exports = router;
