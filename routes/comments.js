3717ec3ed2cc3bfa9b991ee15231aae9b9ea7e0c
BayLadyCoder <rachada_32@hotmail.com>
2024-03-09 22:12:35 -0500

    Fix PUT comments route validations

--- routes/comments.js
+++ routes/comments.js
@@ -26,11 +26,10 @@ router.put(
     body('comment', 'Maximum 500 characters for a comment.')
       .trim()
       .isLength({ min: 1, max: 500 }),
-    body('authorId', 'authorId is  required.').notEmpty(),
-    body('postId', 'postId is required.').notEmpty(),
   ],
   commentsController.updateComment
 );
+
 router.delete('/comments/:commentId', isAuth, commentsController.deleteComment);
 
 module.exports = router;
