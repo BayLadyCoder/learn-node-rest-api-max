8922160e17c9e9b3ceee5e3c103c6aa99b9b64f5
BayLadyCoder <rachada_32@hotmail.com>
2024-03-10 20:08:48 -0400

    Add update comment controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,6 +2,7 @@ const Comment = require('../models/comment');
 const User = require('../models/user');
 const Post = require('../models/post');
 const io = require('../socket/io');
+const { validationResult } = require('express-validator');
 
 exports.createComment = async (req, res, next) => {
   const { comment, authorId, postId } = req.body;
@@ -49,6 +50,40 @@ exports.deleteComment = async (req, res, next) => {
     res.status(200).send({
       message: 'Comment deleted successfully',
       comment: deletedComment,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateComment = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is invalid.');
+    error.statusCode = 422;
+    error.errors = errors.array();
+    return next(error);
+  }
+
+  const { commentId } = req.params;
+  const newComment = req.body.comment;
+
+  try {
+    const comment = await Comment.findById(commentId);
+    const userId = req.userId;
+
+    if (comment.author._id !== userId) {
+      return res.status(403).send({
+        message: "Only comment's author is allowed to update this comment",
+      });
+    }
+
+    comment.comment = newComment;
+    const updatedComment = await comment.save();
+
+    res.status(200).send({
+      message: 'Comment updated successfully',
+      comment: updatedComment,
     });
   } catch (err) {
     next(err);
