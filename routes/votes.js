c8faf1300703e1168d9992d505bab7ad32201087
BayLadyCoder <rachada_32@hotmail.com>
2024-03-12 20:34:37 -0400

    Add PUT /votes route for submitting new vote

--- routes/votes.js
+++ routes/votes.js
@@ -8,4 +8,15 @@ const router = express.Router();
 
 router.get('/votes', isAuth, votesController.getUserVotes);
 
+router.put(
+  '/votes',
+  isAuth,
+  [
+    body('isUpVote', 'isUpVote is required.').notEmpty(),
+    body('userId', 'userId is required.').notEmpty(),
+    body('postId', 'postId is required.').notEmpty(),
+  ],
+  votesController.updateOrCreateVote
+);
+
 module.exports = router;
