6288c643d53d1fc68b3bd52eb140596c5efbd02e
BayLadyCoder <rachada_32@hotmail.com>
2024-03-13 20:22:17 -0400

    Add updateOrCreateVote controller

--- controllers/votes.js
+++ controllers/votes.js
@@ -1,7 +1,72 @@
 const User = require('../models/user');
+const Vote = require('../models/vote');
+const Post = require('../models/post');
 
 exports.getUserVotes = async (req, res, next) => {
   const user = await User.findById(req.userId).populate('votes');
 
   res.status(200).send({ votes: user.votes });
+};
+
+exports.updateOrCreateVote = async (req, res, next) => {
+  try {
+    const { isUpVote, userId, postId, _id } = req.body;
+    let vote;
+
+    const user = await User.findById(userId).populate('votes');
+    const post = await Post.findById(postId);
+
+    if (!user || !post) {
+      const value = !user ? 'user' : 'post';
+      const error = new Error(`Invalid ${value} id`);
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    if (_id) {
+      vote = await Vote.findById(_id);
+
+      if (!vote) {
+        const error = new Error('Invalid vote id');
+        error.statusCode = 404;
+        return next(error);
+      }
+
+      const previousVoteIsUpVote = vote.isUpVote;
+      if (previousVoteIsUpVote === isUpVote) {
+        return res.status(200).send({ message: 'Voted successfully', vote });
+      }
+
+      vote.isUpVote = isUpVote;
+
+      user.votes = user.votes.map((v) => {
+        if (v._id.toString() === vote._id.toString()) {
+          return vote;
+        }
+        return v;
+      });
+
+      if (previousVoteIsUpVote && !isUpVote) {
+        post.votingScores = post.votingScores - 2;
+      } else if (!previousVoteIsUpVote && isUpVote) {
+        post.votingScores = post.votingScores + 2;
+      }
+    } else {
+      vote = new Vote({ isUpVote, userId, postId });
+
+      user.votes.push(vote);
+
+      post.votingScores = isUpVote
+        ? post.votingScores + 1
+        : post.votingScores - 1;
+    }
+
+    await vote.save();
+    await user.save();
+    await post.save();
+
+    res.status(200).send({ message: 'Voted successfully', vote });
+  } catch (err) {
+    next(err);
+  }
 };
